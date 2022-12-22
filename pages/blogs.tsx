1f8506bd3f3022497f5e95a9278ba76e5761bb54
moez-rd <sloe9ie@gmail.com>
2022-12-22 12:51:02 +0700

    Add API

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -3,8 +3,60 @@ import NavLink from '@/components/NavLink'
 import PageTitle from '@/components/PageTitle'
 import { NextPage } from 'next'
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
 
-const Blogs: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
+  // Fetch data from external API
+  const res = await fetch(`https://dev.to/api/articles?username=bzz`)
+  const data: Post[] = await res.json()
+  console.log(data)
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+interface Post {
+  type_of: string
+  id: number
+  title: string
+  description: string
+  readable_publish_date: string
+  slug: string
+  path: string
+  url: string
+  comments_count: number
+  public_reactions_count: number
+  collection_id: number | null
+  published_timestamp: Date
+  positive_reactions_count: number
+  cover_image: string | null
+  social_image: string
+  canonical_url: string
+  created_at: Date
+  edited_at: Date
+  crossposted_at: Date | null
+  published_at: Date
+  last_comment_at: Date
+  reading_time_minutes: number
+  tag_list: string[]
+  tags: string[]
+  user: {
+    name: string
+    username: string
+    twitter_username: string | null
+    github_username: string | null
+    user_id: number
+    website_url: string | null
+    profile_image: string
+    profile_image_90: string
+  }
+}
+
+interface Props {
+  data: Post[]
+}
+
+const Blogs: NextPage<Props> = (props: Props) => {
   return (
     <ContentLayout>
       <Head>
@@ -38,6 +90,7 @@ const Blogs: NextPage = () => {
           <span className="font-bold text-secondary-500">- I currently </span>
           don&apos;t have blog
         </p>
+        <p>{props.data[0].canonical_url}</p>
       </main>
     </ContentLayout>
   )
